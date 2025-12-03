17847e2da2a937811f9ce0e456060a91b2be687c
opmile <milenaoli2703@gmail.com>
2025-12-03 08:57:34 -0300

    chore: base config

---
+++ src/main/java/com/opmile/book_catalog_git/BookCatalogGitApplication.java
@@ -0,0 +1,13 @@
+package com.opmile.book_catalog_git;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookCatalogGitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookCatalogGitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/opmile/book_catalog_git/BookCatalogGitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.opmile.book_catalog_git;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookCatalogGitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
